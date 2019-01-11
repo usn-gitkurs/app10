f8a00b19f16c926479f6b639a44d95d54c6c4d95
Markus W. Byremo <byremo93@hotmail.com>
2019-01-11 12:04:13 +0100

    Login
    Lagt til innlogging, også en signup side

---
+++ SignUp.php
@@ -0,0 +1,13 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SignUp</title>
+	<meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="app.css">
+</head>
+<body>
+
+</body>
+</html>
